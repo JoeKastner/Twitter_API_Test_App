e3bd721a92ec16ceaf3327c9a33aeb08d6cfcf7a
JoeKastner <kastner318@gmail.com>
2020-03-07 21:55:13 -0500

    added chart to see upvotes and number of comments for threads by author

--- reddit.py
+++ reddit.py
@@ -11,6 +11,7 @@ import praw
 import collections
 import pandas as pd
 import json
+import datetime
 
 #%%
 with open('passwords.json') as f:
@@ -23,26 +24,48 @@ reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent='my user agent')
 
-# %%
 comments = collections.defaultdict(list)
-for comment in reddit.subreddit('all').search(query='Con Edison'):
+for comment in reddit.subreddit('all').search(query='author:StringCheeseInc', limit=100, time_filter='all', sort='new'):
     comments['subreddit'].append(comment.subreddit.display_name)
     comments['title'].append(comment.title)
     comments['author'].append(comment.author)
     comments['linked_url'].append(comment.url)
     comments['reddit_url'].append('https://www.reddit.com' + comment.permalink)
     comments['upvotes'].append(comment.ups)
+    comments['downvotes'].append(comment.downs)
+    comments['sumvotes'].append(comment.ups - comment.downs)
     comments['total_awards_received'].append(comment.total_awards_received)
     comments['num_comments'].append(comment.num_comments)
     comments['subreddit_name_prefixed'].append(comment.subreddit_name_prefixed)
     comments['domain'].append(comment.domain)
     comments['subreddit_subscribers'].append(comment.subreddit_subscribers)
-    comments['created_utc'].append(comment.created_utc)
-
+    comments['created_utc'].append(datetime.datetime.utcfromtimestamp(comment.created_utc))
+                                   
 df = pd.DataFrame.from_dict(comments)
 
+import seaborn as sns 
+import matplotlib.pyplot as plt
+sns.set(style="whitegrid", 
+        rc={"lines.linewidth": 4,
+            "figure.figsize":(10, 8)})
+
+
+sns.lineplot(x = df.created_utc, y = df.upvotes, color='#069bd7', alpha=0.75, marker="o", markersize=7)
+sns.lineplot(x = df.created_utc, y = df.num_comments, color='orange', alpha=0.75, marker="o", markersize=7)
+plt.legend(labels=['Upvotes', '# Comments'])
+plt.ylabel("")
+plt.xlabel("Post Date")
+plt.tick_params(labelsize=12)
+plt.title("Upvotes and Comments over time for " + comment.author.name, size=16)
+
+
+# %%
 
 
 
+# sns.scatterplot(x=df.upvotes, y=df.num_comments)
+
+# %%
+sns.PairGrid(df)
 
 # %%
